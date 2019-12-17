966d7d7c3e87c26a659cd1b55adbc61127d1a71a
Josh Lane <jolan@microsoft.com>
2019-12-17 16:00:29 -0600

    WIP for program actors

---
+++ ProgramActor/index.js
@@ -0,0 +1,32 @@
+﻿const df = require("durable-functions");
+
+module.exports = df.entity(function(context) {
+
+    const input = context.df.getInput();
+
+    const programType = input.programType;
+    const instanceId = input.instanceId;
+
+    const state = context.df.getState(getDefaultState(programType));
+    
+    switch (context.df.operationName) {
+        case "datasetEvent":
+            onDatasetEvent(state, input);
+            break;
+        case "timerEvent":
+            onTimerEvent(state, input);
+            break;
+    }
+});
+
+function onDatasetEvent(state, input) {
+
+}
+
+function onTimerEvent(state, input) {
+
+}
+
+function getDefaultState(programType) {
+
+}
