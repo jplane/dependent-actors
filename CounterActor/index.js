4a5c6406c74a2256cf7fa102355e207ac4fbfce4
Josh Lane <jolan@microsoft.com>
2019-12-17 15:08:24 -0600

    initial add

---
+++ CounterActor/index.js
@@ -0,0 +1,19 @@
+﻿const df = require("durable-functions");
+
+module.exports = df.entity(function(context) {
+
+    const currentValue = context.df.getState(() => 0);
+    
+    switch (context.df.operationName) {
+        case "add":
+            const amount = context.df.getInput();
+            context.df.setState(currentValue + amount);
+            break;
+        case "reset":
+            context.df.setState(0);
+            break;
+        case "get":
+            context.df.return(currentValue);
+            break;
+    }
+});
---
+++ FrontDoor/index.js
@@ -0,0 +1,14 @@
+﻿const df = require("durable-functions");
+
+module.exports = async function (context) {
+
+    const instanceId = context.req.params.instanceId;
+
+    const actorProxy = df.getClient(context);
+
+    const entityId = new df.EntityId("CounterActor", instanceId);
+    
+    await actorProxy.signalEntity(entityId, "add", 1);
+
+    return actorProxy.createCheckStatusResponse(context.req, instanceId);
+};
